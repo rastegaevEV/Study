533520a249e82a8ca03adfbcf139ca57a46c0a0a
mause89 <nsk-pto@mail.ru>
2019-02-25 11:19:14 +0700

    Range. Исправил пересечение и объединение.

--- Range/src/ru/rastegaev/range/Range.java
+++ Range/src/ru/rastegaev/range/Range.java
@@ -42,30 +42,21 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if ((range.from >= this.from) && (range.from <= this.to) && (range.to >= this.to)) {
-            return new Range(range.from, this.to);
-        } else if ((this.from >= range.from && this.from <= range.to) && (this.to < range.from || this.to > range.to)) {
-            return new Range(this.from, range.to);
-        } else if ((range.from >= this.from && range.from <= this.to) && (range.to >= this.from && range.to <= this.to)) {
-            return new Range(range.from, range.to);
-        } else if (range.from <= this.from && range.to >= this.to) {
-            return new Range(this.from, this.to);
-        } else {
+        double from = Math.max(this.from, range.from);
+        double to = Math.min(this.to, range.to);
+
+        if (from >= to) {
             return null;
+        } else {
+            return new Range(from, to);
         }
     }
 
     public Range[] getUnion(Range range) {
-        if ((range.from >= this.from) && (range.from <= this.to) && (range.to >= this.to)) {
-            return new Range[]{new Range(this.from, range.to)};
-        } else if ((this.from >= range.from && this.from <= range.to) && (this.to < range.from || this.to > range.to)) {
-            return new Range[]{new Range(range.from, this.to)};
-        } else if ((range.from >= this.from && range.from <= this.to) && (range.to >= this.from && range.to <= this.to)) {
-            return new Range[]{new Range(this.from, this.to)};
-        } else if (range.from <= this.from && range.to >= this.to) {
-            return new Range[]{new Range(range.from, range.to)};
-        } else {
+        if (Math.max(this.from, range.from) > Math.min(this.to, range.to)) {
             return new Range[]{new Range(this.from, this.to), new Range(range.from, range.to)};
+        } else {
+            return new Range[]{new Range(Math.min(this.from, range.from), Math.max(this.to, range.to))};
         }
     }
 
