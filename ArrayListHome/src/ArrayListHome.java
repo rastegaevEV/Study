d0ab556543d7814c0e849dcc3d7e42b89e493d11
mause89 <nsk-pto@mail.ru>
2019-03-13 12:25:01 +0700

    ArrayListHome. Исправить

--- ArrayListHome/src/ArrayListHome.java
+++ ArrayListHome/src/ArrayListHome.java
@@ -8,12 +8,30 @@ public class ArrayListHome {
     public static void main(String[] args) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome\\file.txt"))) {
 
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            ArrayList<Integer> numbers = new ArrayList<>();
 
             while (scanner.hasNext()) {
                 numbers.add(scanner.nextInt());
             }
             System.out.println(numbers);
+
+            for(int i = 0; i < numbers.size(); ++i) {
+                if(numbers.get(i) % 2 == 0) {
+                    numbers.remove(i);
+                }
+            }
+            System.out.println(numbers);
+
+            ArrayList<Integer> unicNambers = new ArrayList<>();
+
+            for (int i = 0; i < numbers.size(); ++i) {
+                for (int j = 0; j < numbers.size(); ++j) {
+                    if (numbers.get(i).equals(unicNambers.get(j))) {
+                        unicNambers.add(numbers.get(i));
+                    }
+                }
+            }
+            System.out.println(unicNambers);
         }
     }
 }
