273d8fc43b1f4bc1f5577c0c3c5acb1598196975
mause89 <nsk-pto@mail.ru>
2019-03-15 11:56:52 +0700

    ArrayListHome. Исправил

--- ArrayListHome/src/ru/rastegaev/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/rastegaev/arraylisthome/ArrayListHome.java
@@ -3,38 +3,39 @@ package ru.rastegaev.arraylisthome;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome\\file.txt"))) {
+    public static void main(String[] args) {
+        try {
+            try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome\\file.txt"))) {
+                ArrayList<String> numbers = new ArrayList<>();
 
-            ArrayList<Integer> numbers = new ArrayList<>();
-
-            while (scanner.hasNext()) {
-                numbers.add(scanner.nextInt());
-            }
-            System.out.println(numbers);
-
-            for (int i = 0; i < numbers.size(); ++i) {
-                if (numbers.get(i) % 2 == 0) {
-                    numbers.remove(i);
-                    --i;
+                while (scanner.hasNext()) {
+                    numbers.add(scanner.nextLine());
                 }
+                System.out.println(numbers.toString().replace(",", ""));
             }
-            System.out.println(numbers);
-
-            ArrayList<Integer> uniqueNumbers = new ArrayList<>(numbers);
-
-            for (int i = 0; i < uniqueNumbers.size(); ++i) {
-                for (int j = i; j < uniqueNumbers.size(); ++j) {
-                    if (uniqueNumbers.get(i).equals(uniqueNumbers.get(j)) && i != j) {
-                        uniqueNumbers.remove(j);
-                        --j;
-                    }
-                }
-            }
-            System.out.println(uniqueNumbers);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
         }
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 4, 3, 2, 1, 5, 3, 8, 9, 9, 7));
+        for (int i = 0; i < numbers.size(); ++i) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
+                --i;
+            }
+        }
+        System.out.println(numbers);
+
+        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (!uniqueNumbers.contains(number)) {
+                uniqueNumbers.add(number);
+            }
+        }
+        System.out.println(uniqueNumbers);
     }
 }
