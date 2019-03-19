c551d789fc4d8090b5f9ead7413c5eaeaa83f328
mause89 <nsk-pto@mail.ru>
2019-03-19 17:58:08 +0700

    Matrix.

--- Matrix/src/ru/rastegaev/matrix/Matrix.java
+++ Matrix/src/ru/rastegaev/matrix/Matrix.java
@@ -1,29 +1,36 @@
 package ru.rastegaev.matrix;
 
-import java.util.Arrays;
-
 public class Matrix {
-    private Vector[] vectors;
+    private Vector[] vector;
 
     public Matrix (int n, int m) {
-        vectors = new Vector[n];
+        this.vector = new Vector[n];
         for (int i = 0; i < n; ++i) {
-            vectors[i] = new Vector(m);
+            vector[i] = new Vector(m);
         }
     }
 
     public Matrix (Matrix matrix) {
-        this.vectors = new Vector[matrix.vectors.length];
-        for (int i = 0; i < vectors.length; ++i) {
-            //todo доделать
+        this.vector = new Vector[matrix.vector.length];
+        for (int i = 0; i < vector.length; ++i) {
+            this.vector[i] = new Vector(matrix.vector[i]);
+        }
+    }
+
+    public Matrix (double[][] numbers) {
+        this.vector = new Vector[numbers.length];
+        for (int i = 0; i < numbers.length; ++i) {
+            for (int j = 0; j < numbers[i].length; ++j) {
+                //todo this.vector[i].setComponent(this.vector[i].getComponent(i), numbers[i][j]);
+            }
         }
     }
 
     public Vector[] getVectorsArray() {
-        return vectors;
+        return vector;
     }
 
     public void setVectors(int index, Vector vector) {
-        this.vectors[index] = vector;
+        this.vector[index] = vector;
     }
 }
--- Matrix/src/ru/rastegaev/matrix/main/Main.java
+++ Matrix/src/ru/rastegaev/matrix/main/Main.java
@@ -9,5 +9,10 @@ public class Main {
             System.out.println(matrix.getVectorsArray()[i].toString());
         }
 
+        Matrix matrix1 = new Matrix(new double[][]{{1, 2, 3}, {4, 5, 6}});
+        for (int i = 0; i < matrix.getVectorsArray().length; ++i) {
+            System.out.println(matrix1.getVectorsArray()[i].toString());
+        }
+
     }
 }
