77a1e6941b3a093cbb85ca71df64991ed6419b1d
mause89 <nsk-pto@mail.ru>
2019-03-20 12:19:08 +0700

    Matrix. Добавил методы

--- Matrix/src/ru/rastegaev/matrix/Matrix.java
+++ Matrix/src/ru/rastegaev/matrix/Matrix.java
@@ -3,27 +3,36 @@ package ru.rastegaev.matrix;
 public class Matrix {
     private Vector[] vector;
 
-    public Matrix (int n, int m) {
+    public Matrix(int n, int m) {
         this.vector = new Vector[n];
         for (int i = 0; i < n; ++i) {
             vector[i] = new Vector(m);
         }
     }
 
-    public Matrix (Matrix matrix) {
+    public Matrix(Matrix matrix) {
         this.vector = new Vector[matrix.vector.length];
         for (int i = 0; i < vector.length; ++i) {
             this.vector[i] = new Vector(matrix.vector[i]);
         }
     }
 
-    public Matrix (double[][] numbers) {
+    public Matrix(double[][] numbers) {
         this.vector = new Vector[numbers.length];
         for (int i = 0; i < numbers.length; ++i) {
-            for (int j = 0; j < numbers[i].length; ++j) {
-                //todo this.vector[i].setComponent(this.vector[i].getComponent(i), numbers[i][j]);
-            }
+            this.vector[i] = new Vector(numbers[i]);
         }
+    }
+
+    public Matrix(Vector[] vectors) {
+        this.vector = new Vector[vectors.length];
+        for (int i = 0; i < vectors.length; ++i) {
+            this.vector[i] = new Vector(vectors[i]);
+        }
+    }
+
+    public int getSize() {
+        return vector.length; //todo
     }
 
     public Vector[] getVectorsArray() {
@@ -33,4 +42,29 @@ public class Matrix {
     public void setVectors(int index, Vector vector) {
         this.vector[index] = vector;
     }
+
+    public Vector getColumnVector(int index) {
+        Vector columnVector = new Vector(this.vector.length);
+        for (int i = 0; i < this.vector.length; ++i) {
+            columnVector.setComponent(i, this.vector[i].getComponent(index));
+        }
+        return columnVector;
+    }
+
+    public void multiplicationOnScalar(int scalar) {
+        for (int i = 0; i < this.vector.length; ++i) {
+            for (int j = 0; j < this.vector[i].getSize(); ++j) {
+                this.vector[i].setComponent(j, this.vector[i].getComponent(j) * scalar);
+            }
+        }
+    }
+
+    public void multiplicationOnVector (Vector vector) {
+        for (int i = 0; i < this.vector.length; ++i) {
+            for (int j = 0; j < this.vector[i].getSize(); ++j) {
+                this.vector[i].setComponent(j, this.vector[i].getComponent(j) * vector.getComponent(j));
+            }
+        } //todo адаптировать все входящие векторы(данные) под длинну массива this.vector!
+    }
+
 }
--- Matrix/src/ru/rastegaev/matrix/main/Main.java
+++ Matrix/src/ru/rastegaev/matrix/main/Main.java
@@ -1,6 +1,7 @@
 package ru.rastegaev.matrix.main;
 
 import ru.rastegaev.matrix.Matrix;
+import ru.rastegaev.matrix.Vector;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +9,35 @@ public class Main {
         for (int i = 0; i < matrix.getVectorsArray().length; ++i) {
             System.out.println(matrix.getVectorsArray()[i].toString());
         }
-
+        System.out.println();
         Matrix matrix1 = new Matrix(new double[][]{{1, 2, 3}, {4, 5, 6}});
-        for (int i = 0; i < matrix.getVectorsArray().length; ++i) {
+        for (int i = 0; i < matrix1.getVectorsArray().length; ++i) {
+            System.out.println(matrix1.getVectorsArray()[i].toString());
+        }
+        System.out.println();
+        Matrix matrix2 = new Matrix(matrix);
+        for (int i = 0; i < matrix2.getVectorsArray().length; ++i) {
+            System.out.println(matrix2.getVectorsArray()[i].toString());
+        }
+        System.out.println();
+        Vector[] vectors = new Vector[]{new Vector(new double[]{3, 5, 7, 8}), new Vector(new double[]{1, 2 ,7})};
+        Matrix matrix3 = new Matrix(vectors);
+        for (int i = 0; i < matrix3.getVectorsArray().length; ++i) {
+            System.out.println(matrix3.getVectorsArray()[i].toString());
+        }
+        System.out.println();
+        System.out.println(matrix3.getColumnVector(1).toString()); // получение столбца
+
+        matrix3.multiplicationOnScalar(2);
+        for (int i = 0; i < matrix3.getVectorsArray().length; ++i) {    // умножение на скаляр
+            System.out.println(matrix3.getVectorsArray()[i].toString());
+        }
+        System.out.println();
+        matrix1.multiplicationOnVector(new Vector(new double[]{2, 2, 2})); // умножение на вектор
+        for (int i = 0; i < matrix1.getVectorsArray().length; ++i) {
             System.out.println(matrix1.getVectorsArray()[i].toString());
         }
 
+        }
     }
-}
+
