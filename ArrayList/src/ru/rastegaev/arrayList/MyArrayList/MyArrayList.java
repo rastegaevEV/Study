4b06bdc42b7f6d8595787fd0cb5c97f155f5e02a
pto <nsk-pto@mail.ru>
2019-05-29 17:26:25 +0700

    MyArrayList. Исправил замечания

--- ArrayList/src/ru/rastegaev/arrayList/MyArrayList/MyArrayList.java
+++ ArrayList/src/ru/rastegaev/arrayList/MyArrayList/MyArrayList.java
@@ -110,7 +110,7 @@ public class MyArrayList<T> implements List<T> {
     }
 
     public void trimToSize() {
-        if (this.items.length < size) {
+        if (this.items.length < this.size) {
             this.items = Arrays.copyOf(this.items, size);
         }
     }
