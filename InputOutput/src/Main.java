a429d52677eecc232e1ae0a527310171b4568fa4
pto <nsk-pto@mail.ru>
2019-10-04 17:56:05 +0700

    IO

--- InputOutput/src/Main.java
+++ InputOutput/src/Main.java
@@ -1,14 +1,26 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (InputStream inFile = new FileInputStream("in.txt")) {
-            int read = 0;
+        try (Scanner inFile = new Scanner(new FileInputStream("C:\\Users\\pto\\IdeaProjects\\JavaOOP\\InputOutput\\in.txt"))) {
+            while (inFile.hasNext()) {
+                String f = inFile.nextLine();
+                System.out.println(f);
+            }
+        }
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("C:\\Users\\pto\\IdeaProjects\\JavaOOP\\InputOutput\\in.txt"))) {
+            int read;
             int off = 0;
-            byte[] array = new byte[1000000];
-            while ((read = inFile.read(array, off, array.length - off)) != -1) {
+            byte[] array = new byte[100];
+            while ((read = inputStream.read(array, off, array.length - off)) != -1) {
                 off += read;
             }
+            System.out.println(Arrays.toString(array));
+
+            OutputStream stream = new FileOutputStream("C:\\Users\\pto\\IdeaProjects\\JavaOOP\\InputOutput\\out.txt");
+            stream.write(array);
         }
     }
 }
