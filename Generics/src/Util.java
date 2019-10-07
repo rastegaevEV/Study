8d86266c9767b1312f91e80c911293e0f33ac994
pto <nsk-pto@mail.ru>
2019-10-07 16:05:33 +0700

    Deleting

--- Generics/src/Util.java
+++
@@ -1,5 +0,0 @@
-public class Util {
-    public static <K, V> boolean compare(Pair<K, V> first, Pair<K, V> second) {
-        return first.getFirst().equals(second.getFirst()) && first.getSecond().equals(second.getSecond());
-    }
-}
